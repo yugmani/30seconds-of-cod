29e58051816b44c5939a3aaf62ac37d8d7ab174d
yugmani <yugmani@yahoo.com>
2021-06-17 13:27:58 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# 30seconds-of-cod
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/30seconds-of-cod)
---
+++ script.js
@@ -0,0 +1,9 @@
+// **********************************
+// How do I convert an iterable to an array in JavaScript?
+// ***********************************
+
+// use the spread operator to convert iterables or, as they are sometimes referred to, array-likes. 
+// the spread operator (...) allows an iterable to be expanded in places where zero or more arguments or elements are expected.
+
+// 1 STRING: 
+// spread operator is used to make an array of strings each one representing a character of the original string:
