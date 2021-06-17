ac94b4861e07dc6fbae2c6401292239c2a675599
yugmani <yugmani@yahoo.com>
2021-06-17 16:13:42 -0700

    make any javascript value iterable

--- script.js
+++ script.js
@@ -10,7 +10,7 @@
 
 const name = 'spiral';
 const letters = [...name];
-console.log('Letters of name:' + letters);
+// console.log('Letters of name:' + letters);
 // Expected: Letters of name:s,p,i,r,a,l
 
 // 2. NODELIST
@@ -20,32 +20,45 @@ console.log('Letters of name:' + letters);
 
 const nodes = document.body.childNodes;
 // console.log(nodes); // NodeList(6) [text, h1, text, ol, text, script]
-// 0: text
-// 1: h1
-// 2: text
-// 3: ol
-// 4: text
-// 5: script
-// 6: text
-// length: 7
+
 // console.log(typeof nodes);  // object
-nodes.forEach(item=>{
-  console.log(item);
-})
+
+// console.log('Is nodes an array?: ' + Array.isArray(nodes)); // Is nodes an array?: false
 
 const nodeArray = [...nodes];
 // console.log(nodeArray); // (6) [text, h1, text, ol, text, script]
-// 0: text
-// 1: h1
-// 2: text
-// 3: ol
-// 4: text
-// 5: script
-// length: 6
+
 // console.log(typeof nodeArray);  // object
 
-nodeArray.forEach(item=>{
-  console.log(item);
-})
+// console.log('Is nodeArray an array?: ' + Array.isArray(nodeArray)); // Is nodeArray an array?: true
 
 // console.log(nodes === nodeArray); // false
+
+// **********************************
+// Make any JavaScript value iterable
+// ********************************************
+
+// JavaScript's `Symbol.iterator` is a very powerful tool that every web developer should learn how to use.
+// It allows you to define and customize the way a value is iterated, effectively allowing you to make any value iterable.
+// You can easily apply this knowledge to plain JavaScript objects and even classes.
+
+// All you need to correctly define an iterator is a generator function yielding each of the iteration values.
+// This could be used to retrieve key-value pairs in an object, call specific getter functions from a class or split a number into an array of digits:
+
+const obj = { a: 1, b: 2, c: 3 };
+// console.log([...obj]);  // TypeError: obj is not iterable
+
+obj[Symbol.iterator] = function*() {
+  for (let key of Object.keys(obj)) yield { [key]: obj[key] };
+};
+
+// console.log([...obj]);  // [{a:1}, {b:2}, {c:3}]
+
+class IterableNumber extends Number {
+  *[Symbol.iterator]() {
+    for (let digit of [...`${this}`].map(d => Number.parseInt(d))) yield digit;
+  }
+}
+
+const num = new IterableNumber(1337);
+// console.log([...num]);  // (4) [1, 3, 3, 7]
